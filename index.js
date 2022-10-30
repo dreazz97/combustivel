c74ab6523deef01c08e6ca5854b30755f9a376e1
dreazz97 <iuripeniche@hotmail.com>
2022-10-30 01:08:51 +0100

    ajustes

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let seixal_fuel_title_data = [];
 let seixal_fuel_price_data = [];
+let seixal_fuel_type_data = [];
 let location_lat = [];
 let location_lon = [];
 let item_clicker = [];
 
-async function getFuelData() {
-    const seixal_fuel_response = await fetch('https://precoscombustiveis.dgeg.gov.pt/api/PrecoComb/PesquisarPostos?idsTiposComb=2105&idMarca=&idTipoPosto=&idDistrito=&idsMunicipios=229&qtdPorPagina=50&pagina=1');
+
+  async function getFuelData() {
+    var seixal_fuel_response = await fetch('https://precoscombustiveis.dgeg.gov.pt/api/PrecoComb/PesquisarPostos?idsTiposComb=2105&idMarca=&idTipoPosto=&idDistrito=&idsMunicipios=229&qtdPorPagina=50&pagina=1');
+
     const seixal_fuel_data = await seixal_fuel_response.json();
     console.log(seixal_fuel_data);
     console.log(seixal_fuel_data.resultado[0].Nome);
@@ -13,14 +16,30 @@ async function getFuelData() {
     for (let i = 0; i < 5; i++) {
      seixal_fuel_title_data[i] = seixal_fuel_data.resultado[i].Nome;
      seixal_fuel_price_data[i] = seixal_fuel_data.resultado[i].Preco;
+     seixal_fuel_type_data[i] = seixal_fuel_data.resultado[i].Combustivel; 
      location_lat[i] = seixal_fuel_data.resultado[i].Latitude;
      location_lon[i] = seixal_fuel_data.resultado[i].Longitude;
     }
     seixal_fuel_title_list();
     seixal_fuel_price_list();
+    seixal_fuel_type_list();
     getPostoLocation();
 
 }
+
+
+
+
+
+var click_github = document.querySelector('#github-icon');
+click_github.addEventListener("click", function () {
+  location.href = "https://github.com/dreazz97";
+  })
+
+var click_linkedin = document.querySelector('#linkedin-icon-after');
+  click_linkedin.addEventListener("click", function () {
+    location.href = "https://pt.linkedin.com/in/iuri-peniche-29337a183";
+    })
 
 getFuelData();
 
@@ -43,6 +62,16 @@ function seixal_fuel_price_list () {
     unordered_list_element_posto_preco.appendChild(seixal_fuel_data_price_list_element[i]);
       }
     }
+
+function seixal_fuel_type_list () {
+      var seixal_fuel_data_type_list_element = [];
+      for (let i = 0; i < seixal_fuel_type_data.length; i++) {
+      seixal_fuel_data_type_list_element[i] = document.createElement("li");
+      seixal_fuel_data_type_list_element[i].textContent = seixal_fuel_type_data[i];
+      var unordered_list_element_fuel_type = document.querySelector("#tipo_combustivel");
+      unordered_list_element_fuel_type.appendChild(seixal_fuel_data_type_list_element[i]);
+        }
+      }
 
 function getPostoLocation () {
     var location_element = [];
